16c347b8f642c3b5d9107ddc72881c527141997c
Xulun Huang <xulunh01@gmail.com>
2026-05-31 14:05:11 -0400

    chore: setup docker and knex connection

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+export default {
+  development: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      directory: './src/config/migrations',
+      tableName: 'knex_migrations',
+    },
+  },
+};
